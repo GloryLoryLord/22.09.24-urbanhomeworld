834985ad53885a9b623c8d43c4681e869e135cbb
Ingwar <86135910+GloryLoryLord@users.noreply.github.com>
2024-09-22 22:19:19 +0300

    22.09.24
    заливаю задачу

---
+++ 2) 22.09.24 Project 10/main.py
@@ -0,0 +1,9 @@
+number_tasks = 12
+number_hours = 1.5
+name_uv = 'python'
+one_tasks = number_hours / number_tasks
+print(name_uv,'всего задач:',number_tasks,'затарчено часов:',number_hours, 'среднее время выполнения', one_tasks, 'часа.')
+
+print(name_uv,'всего задач:',number_tasks,',','затрачено часов:',number_hours,',', 'среднее время выполнения', one_tasks, 'часа.')
+
+print(name_uv,'всего задач:',number_tasks,',','затрачено часов:',number_hours,',', 'среднее время выполнения:', one_tasks, 'часа.')
